b55345560670ca7b8f3d49ab1cb4c4086a87c880
Valorie Sours <jamesnsours@gmail.com>
2026-03-27 21:06:48 -0400

    better logging & errors

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 import lark
 import click
 import logging
+import rich
 import tomllib
+import sys
+import os
+from platformdirs import PlatformDirs
 from dataclasses import dataclass
 from definitions import Definitions
 from eval import EvaluationTree
+from logging import handlers
+
+import lark_error_reporting as parse_error_reporting
+
+platform = PlatformDirs("arbaregni", "timep")
+def get_logging_location_name():
+    if not os.path.isdir(platform.user_log_dir):
+        os.makedirs(platform.user_log_dir)
+    return os.path.join(platform.user_log_dir, 'app.log')
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(
-#    format='%(levelname)s: %s(message)s',
+    filename='app.log',
     level=logging.INFO
+)
+handler = logging.handlers.RotatingFileHandler(
+    filename=get_logging_location_name(),
+    backupCount=10
 )
 
 @dataclass
@@ -40,14 +57,21 @@ def setup_context() -> EvalContext:
 
 
 def handle_eval(ctx: EvalContext, expression: str):
+    sources = { 'expression': expression }
     try:
         result = ctx.grammar.parse(expression)
+    except lark.UnexpectedInput as parse_exc:
+        logger.info("Parse error occured", exc_info=True)
+        parse_error_reporting.handle(sources, 'expression', parse_exc)
     except Exception as exc:
+        # TODO: better error JsonFragment::BeginDict)ng
         # TODO: better error handling
-        click.echo("Exception occured: %s" % exc)
+        logger.error("An unexpected error occured: %s" % exc, exc_info=True)
+        click.echo("An unexpected error occured: %s" % exc)
+        click.echo("Check stack trace at %s" % get_logging_location_name())
         return
-
-    click.echo(result)
+    else:
+        rich.print(result)
 
 @click.command()
 @click.argument('expression', default=None)
@@ -68,4 +92,11 @@ def main(expression):
             break
         
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Exception as exc:
+        logger.error("An unexpected error occured: %s" % exc, exc_info=True)
+        click.echo("An unexpected error occured: %s" % exc)
+        click.echo("Check stack trace at %s" % get_logging_location_name())
+        sys.exit(1)
+
